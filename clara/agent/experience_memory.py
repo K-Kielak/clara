97e37a8dc5738be3f75214e5b88b14f10fb461be
Kacper Kielak <kielak.kacper@gmail.com>
2018-02-04 00:18:35 +0000

    Remove debugging code from agent Memory class

--- clara/agent/experience_memory.py
+++ clara/agent/experience_memory.py
@@ -9,7 +9,6 @@ class Memory(object):
     def __init__(self, memory_size):
         self.experiences = deque()
         self.memory_size = memory_size
-        self.samples_given = 0
 
     def add(self, initial_state, action, reward, following_state):
         experience = np.array([initial_state, action, reward, following_state])
@@ -20,18 +19,4 @@ class Memory(object):
 
     def get_samples(self, sample_size):
         samples = random.sample(self.experiences, sample_size)
-        try:
-            self.samples_given += 1
-            return np.reshape(samples, [sample_size, Memory.EXPERIENCE_SIZE])
-        except ValueError:
-            print(self.samples_given)
-            print(len(samples))
-            for s in samples:
-                try:
-                    print(len(s))
-                    print(len(s[0]))
-                    print(s[1])
-                    print(s[2])
-                    print(len(s[3]))
-                except IndexError:
-                    print(s)
+        return np.reshape(samples, [sample_size, Memory.EXPERIENCE_SIZE])
